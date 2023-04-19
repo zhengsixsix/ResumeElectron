518efcbf0c5a9ee81e9e3a6d5556038ef2c00caf
zhengsixsix <1833496831@qq.com>
2023-04-19 20:17:22 +0800

    feat: 新增添加未选模块已选模块的逻辑

--- app/renderer/common/constants/resume.ts
+++ app/renderer/common/constants/resume.ts
@@ -34,48 +34,48 @@ export const RESUME_TOOLBAR_LIST: TSResume.SliderItem[] = [
         key: RESUME_TOOLBAR_MAPS.workPrefer,
         name: '工作期望',
         summary: '聊聊你所期望的宜人办公城市',
-        require: true
+        require: false
     },
     {
         key: RESUME_TOOLBAR_MAPS.schoolExperience,
         name: '在校经历',
         summary: '展示在校任职成果和人际关系',
-        require: true
+        require: false
     },
     {
         key: RESUME_TOOLBAR_MAPS.projectExperience,
         name: '项目经验',
         summary: '展示研究过什么优秀项目和成果',
-        require: true
+        require: false
     },
     {
         key: RESUME_TOOLBAR_MAPS.workExperience,
         name: '工作经历',
         summary: '申请岗位的相关经验和能力',
-        require: true
+        require: false
     },
     {
         key: RESUME_TOOLBAR_MAPS.certificate,
         name: '获奖证书',
         summary: '得过什么奖项值得炫耀',
-        require: true
+        require: false
     },
     {
         key: RESUME_TOOLBAR_MAPS.evaluation,
         name: '个人评价',
         summary: '低调夸一夸自己有什么亮点',
-        require: true
+        require: false
     },
     {
         key: RESUME_TOOLBAR_MAPS.skill,
         name: '技能清单',
         summary: '展示具备的技能，突出你的能力',
-        require: true
+        require: false
     },
     {
         key: RESUME_TOOLBAR_MAPS.test,
         name: '测试用例',
         summary: '测试用例，一键覆盖',
-        require: true
+        require: false
     },
 ]
--- app/renderer/container/resume/ResumeToolbar/index.tsx
+++ app/renderer/container/resume/ResumeToolbar/index.tsx
@@ -1,18 +1,84 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ToolbartLess from './index.less';
 import { RESUME_TOOLBAR_LIST } from '@common/constants/resume';
 import Scroll from '@common/components/MyScroll';
 export default function ResumeToolbar() {
   const height = document.body.clientHeight;
+  // 👇 定义已添加模块与未添加模块
+  const [addToobarList, setAddToobarList] = useState<TSResume.SliderItem[]>([]);
+  const [unToobarList, setUnToobarList] = useState<TSResume.SliderItem[]>([]);
+  useEffect(() => {
+    if (RESUME_TOOLBAR_LIST.length > 0) {
+      let _addToobarList: TSResume.SliderItem[] = [];
+      let _unToobarList: TSResume.SliderItem[] = [];
+      RESUME_TOOLBAR_LIST.forEach((item: TSResume.SliderItem) => {
+        if (item.require) {
+          _addToobarList.push(item);
+        } else {
+          _unToobarList.push(item);
+        }
+        setAddToobarList(_addToobarList);
+        setUnToobarList(_unToobarList);
+      });
+    }
+  }, []);
+  const onAddSliderAction = (moduleToolbar: TSResume.SliderItem) => {
+    // 1. 获取已添加模块的所有 key 值
+    const addKeysList = addToobarList.map((item: TSResume.SliderItem) => item.key);
+    let nextAddToolbarList = [...Array.from(addToobarList)];
+    // 2. 如果未包含当前要添加的模块key，则加入
+    if (!addKeysList.includes(moduleToolbar.key)) {
+      nextAddToolbarList = nextAddToolbarList.concat(moduleToolbar);
+    }
+
+    setAddToobarList(nextAddToolbarList);
+    const nextUnAddToolbarList = [...Array.from(unToobarList)];
+    const findIndex = nextUnAddToolbarList.findIndex((item) => item.key === moduleToolbar.key);
+    if (findIndex > -1) {
+      nextUnAddToolbarList.splice(findIndex, 1);
+    }
+    setUnToobarList(nextUnAddToolbarList);
+  };
   return (
     <div className={ToolbartLess.slider}>
       <Scroll maxHeight={height - 180}>
         <div className={ToolbartLess.module}>
-          全部模块
+          <div className={ToolbartLess.title}>
+            已添加模块
+            <div className={ToolbartLess.line} />
+          </div>
           <div className={ToolbartLess.content}>
-            {RESUME_TOOLBAR_LIST.map((ITEM: TSResume.SliderItem) => {
+            {addToobarList.map((ITEM: TSResume.SliderItem) => {
               return (
                 <div className={ToolbartLess.box} key={ITEM.key}>
+                  <div className={ToolbartLess.info}>
+                    <i className={ToolbartLess.info} />
+                    <div className={ToolbartLess.text}>
+                      <div className={ToolbartLess.name}>{ITEM.name}</div>
+                      <div className={ToolbartLess.summary}>{ITEM.summary}</div>
+                    </div>
+                    {ITEM.require && <div className={ToolbartLess.tips}>必选项</div>}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className={ToolbartLess.module}>
+          <div className={ToolbartLess.title}>
+            未添加模块
+            <div className={ToolbartLess.line} />
+          </div>
+          <div className={ToolbartLess.content}>
+            {unToobarList.map((ITEM: TSResume.SliderItem) => {
+              return (
+                <div
+                  className={ToolbartLess.box}
+                  key={ITEM.key}
+                  onClick={() => {
+                    onAddSliderAction(ITEM);
+                  }}
+                >
                   <div className={ToolbartLess.info}>
                     <i className={ToolbartLess.info} />
                     <div className={ToolbartLess.text}>
